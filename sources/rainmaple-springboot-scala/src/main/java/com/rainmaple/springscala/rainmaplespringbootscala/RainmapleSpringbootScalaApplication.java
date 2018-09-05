cbb8e10627f1e8140043e3361cba1ea22d0f0434
Tyisadmin <1205022533@qq.com>
2018-09-05 20:22:05 +0800

    first commit

---
+++ sources/rainmaple-springboot-scala/src/main/java/com/rainmaple/springscala/rainmaplespringbootscala/RainmapleSpringbootScalaApplication.java
@@ -0,0 +1,12 @@
+package com.rainmaple.springscala.rainmaplespringbootscala;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RainmapleSpringbootScalaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RainmapleSpringbootScalaApplication.class, args);
+    }
+}
---
+++ sources/rainmaple-springboot-scala/src/test/java/com/rainmaple/springscala/rainmaplespringbootscala/RainmapleSpringbootScalaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.rainmaple.springscala.rainmaplespringbootscala;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RainmapleSpringbootScalaApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
